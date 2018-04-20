cde008424b8a182edf63d1df6e036d14bd036de2
Yoshihiro Tanaka <contact@cordea.jp>
2018-04-21 00:15:08 +0900

    parse skills

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,31 @@
 #![feature(proc_macro)]
 
+#[macro_use]
+extern crate serde_derive;
+
 extern crate maud;
+extern crate serde_json;
+
+mod skill;
+
+use std::fs::File;
+use std::io::prelude::*;
 use maud::DOCTYPE;
 use maud::html;
+use skill::Skills;
+
+fn read_file(filename: String) -> String {
+    let mut file = File::open(filename).expect("not found");
+    let mut content = String::new();
+    file.read_to_string(&mut content).expect("");
+
+    return content;
+}
 
 fn main() {
+    let file = read_file("./src/skills.json".to_string());
+    println!("{}", file);
+    let data: Skills = serde_json::from_str(&file).unwrap();
     let introduction = "I am an android applications engineer at the CyberAgent Inc, and live in Tokyo. CyberAgent is my second company and I have been working since Sep 2017. I mainly use Java / Kotlin at work. As a hobby, developing using many languages, using Python, Nim, Golang, C#, JavaScript etc (Please see Skills for details). Native language is Japanese. English uses to read api document and to write commit message, issue, blog, but I am not good at talking.";
 
     let html = html! {
