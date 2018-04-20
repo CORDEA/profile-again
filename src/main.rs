a8490d4a29e5a778923bd13b018f45c5f43e371a
Yoshihiro Tanaka <contact@cordea.jp>
2018-04-20 22:26:11 +0900

    add intro

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use maud::DOCTYPE;
 use maud::html;
 
 fn main() {
+    let introduction = "I am an android applications engineer at the CyberAgent Inc, and live in Tokyo. CyberAgent is my second company and I have been working since Sep 2017. I mainly use Java / Kotlin at work. As a hobby, developing using many languages, using Python, Nim, Golang, C#, JavaScript etc (Please see Skills for details). Native language is Japanese. English uses to read api document and to write commit message, issue, blog, but I am not good at talking.";
+
     let html = html! {
         (DOCTYPE)
         html {
